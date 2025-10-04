b2db7a617e8a3f9519471ddd6885b5d53dd50413
isoumyadas <sammy8das@gmail.com>
2025-10-04 20:21:58 +0000

    Added db conncetion, models and schemas

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// This is written for connection var
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+// now connection var holds the conncetion object and it had only one key as isConnected in which we have handled in try block as if db is connceted then pass the number value to isConnected key in connection object.
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {}); // here in {} we can pass other optional arguments & conosle log this variable (db)
+
+    // conosle log db.connections to check the values & db variable too...
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("DB connected successfully..");
+  } catch (error) {
+    console.log("Database connection is failed ", error);
+
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
---
+++ src/models/User.ts
@@ -0,0 +1,74 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// message is the part of mongoose document, so we have extended Message with Doc
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+// user schema
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[]; // A single user can have multiple Messages.
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"], // This is how we can give the custom messgae.
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"], // This is how we can give the custom messgae.
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"], // This is how we can give the custom messgae.
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"], // This is how we can give the custom messgae.
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+// This mean as nextjs runs on the edge, so it's not aware of are models already created or it's a new model, as we check this we also type checking it if we are getting same data from DB as our User interface.
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
