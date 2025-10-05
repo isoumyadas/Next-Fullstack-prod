03891f3f2cab968d25b48aca8e0922f0b8f692c6
isoumyadas <sammy8das@gmail.com>
2025-10-05 06:58:14 +0000

    fix: removed message param from schemas

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const messageSchema = z.object({
   content: z
     .string()
-    .min(10, { message: "Content must be at least of 10 characters" })
-    .max(300, { message: "Content must be no longer than 300 characters" }),
+    .min(10, "Content must be at least of 10 characters")
+    .max(300, "Content must be no longer than 300 characters"),
 });
--- src/schemas/signupSchema.ts
+++ src/schemas/signupSchema.ts
@@ -3,13 +3,11 @@ import { z } from "zod";
 export const usernameValidation = z
   .string()
   .min(2, "username must be atleast 2 characters")
-  .max(20, "username must be no more than 20 characters")
+  .max(20, "username must be no more than 20 characters") // message key is deprecated.
   .regex(/^[a-zA-Z0-9_]+$/, "username must not contain specail character");
 
 export const signUpSchema = z.object({
   username: usernameValidation,
-  email: z.email({ message: "Invalid email address" }),
-  password: z
-    .string()
-    .min(6, { message: "password must be atleast 6 characters" }),
+  email: z.email("Invalid email address"),
+  password: z.string().min(6, "password must be atleast 6 characters"),
 });
