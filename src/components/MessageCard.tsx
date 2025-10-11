c6d84af0be85f94581194a6578a5e6f49cabbd26
isoumyadas <sammy8das@gmail.com>
2025-10-11 20:05:09 +0000

    feat: added navbar, messageCard and other shadcn ui components

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { X } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  Card,
+} from "./ui/card";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Message } from "@/models/User";
+
+import { toast } from "sonner";
+import axios from "axios";
+
+type MessageCardProps = {
+  message: Message;
+  onMessageDelete: (messageId: string) => void;
+};
+
+export default function MessageCard({
+  message,
+  onMessageDelete,
+}: MessageCardProps) {
+  const handleDeleteConfirm = async () => {
+    const res = await axios.delete(`/api/delete-message/${message._id}`);
+
+    toast.success(res.data.message);
+
+    onMessageDelete(message._id as string); // check this
+  };
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">
+                <X className="w-5 h-5" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteConfirm}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import { User } from "next-auth";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+export default function Navbar() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  const onLogout = async () => {
+    const res = await signOut({ redirect: false });
+
+    if (res) {
+      toast.success("User logged out successfully");
+      router.replace("/sign-in");
+    } else {
+      toast.error("Error while logging out");
+    }
+  };
+
+  const user = session?.user as User;
+
+  return (
+    <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+          True Feedback
+        </a>
+        {session ? (
+          <>
+            <span className="mr-4">
+              Welcome, {user?.username || user?.email}
+            </span>
+            <Button
+              onClick={onLogout}
+              className="w-full md:w-auto bg-slate-100 text-black cursor-pointer"
+              variant="outline"
+            >
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Link href={"/sign-in"}>
+            <Button
+              className="w-full md:w-auto bg-slate-100 text-black cursor-pointer"
+              variant={"outline"}
+            >
+              Login
+            </Button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+}
