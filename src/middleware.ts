93eaea4889e453e115c52f7863065a72c1579901
isoumyadas <sammy8das@gmail.com>
2025-10-15 11:43:22 +0000

    fix: added messages.json and updated middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,14 +28,13 @@ export async function middleware(request: NextRequest) {
     token &&
     (url.pathname.startsWith("/sign-in") ||
       url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify") ||
-      url.pathname === "/")
+      url.pathname.startsWith("/verify"))
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   if (!token && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    return NextResponse.redirect(new URL("/sign-up", request.url));
   }
 
   return NextResponse.next();
